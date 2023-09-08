203462e88f92ff20cdfdab1f91ae1a39e50cf9ba
Dan Frederick <dpfrederick@webstaurantstore.com>
2023-09-08 15:44:07 -0400

    working on traefik

--- docker-run.sh
+++ docker-run.sh
@@ -8,4 +8,4 @@ export PASSWORD=changeme
 export HASHED_PASSWORD=$(openssl passwd -apr1 $PASSWORD)
 
 docker compose -f docker-compose.traefik.yml up -d
-docker compose -f docker-compose.yml up -d
+# docker compose -f docker-compose.yml up -d
