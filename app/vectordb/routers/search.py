2bfbe673bc823cfe5160244f219436ab231a7be8
Dan Frederick <dpfrederick@webstaurantstore.com>
2023-09-12 02:21:03 -0400

    add search endpoints for collections and file classes

--- app/vectordb/routers/search.py
+++ app/vectordb/routers/search.py
@@ -38,3 +38,21 @@ async def search_by_file(file_id: int, query: str, num_results: int = 5):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     return results
+
+@search_router.get("/collections/{collection_id}/search", response_model=SearchResponse, responses=search_responses, tags=['semantic search'])
+async def search_by_collection(collection_id: int, query: str, num_results: int = 5):
+    search = SearchProvider()
+    try:
+        results = search.get_relevant_segments_by_collection_id(query, collection_id, num_results)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return results
+
+@search_router.get("/file-classes/{file_class_id}/search", response_model=SearchResponse, responses=search_responses, tags=['semantic search'])
+async def search_by_file_class(file_class_id: int, query: str, num_results: int = 5):
+    search = SearchProvider()
+    try:
+        results = search.get_relevant_segments_by_file_class_id(query, file_class_id, num_results)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return results
--- app/vectordb/search.py
+++ app/vectordb/search.py
@@ -52,25 +52,33 @@ class SearchProvider:
             self.vectordb.disconnect()
 
     def get_relevant_segments_by_document_id(self, input_query: str, document_id: int, num_segments: int = 5, embedding_column: str = 'embedding'):
-        table_name = "segment"
         embedding_column_name = embedding_column
-        fields = ['id', 'document_id', 'content', embedding_column_name, 'created_at']
-        select_query = "SELECT {fields} from {table} WHERE document_id = %s"
-
+        fields = ['id', 'document_id', 'content', 'created_at']
+        segments_query = """
+            SELECT id, document_id, content, {embedding_column}, created_at
+            FROM segment
+            WHERE document_id = %s
+        """
         search_query = """
-            SELECT {fields}
-            FROM ({select_query}) AS relevant_segments
+            SELECT *
+            FROM ({segments_query}) AS relevant_segments
             ORDER BY {embedding_column} <-> CAST(%s AS vector)
             LIMIT %s
         """
+        select_query = """
+            SELECT {fields}
+            FROM ({search_query}) AS relevant_segments_to_return
+        """
 
-        query = sql.SQL(search_query).format(
+        query = sql.SQL(select_query).format(
             fields=sql.SQL(',').join(map(sql.Identifier, fields)),
-            select_query=sql.SQL(select_query).format(
+            search_query=sql.SQL(search_query).format(
                 fields=sql.SQL(',').join(map(sql.Identifier, fields)),
-                table=sql.Identifier(table_name)
-            ),
-            embedding_column=sql.Identifier(embedding_column_name)
+                segments_query=sql.SQL(segments_query).format(
+                        embedding_column=sql.Identifier(embedding_column_name)
+                ),
+                embedding_column=sql.Identifier(embedding_column_name)
+            )
         )
 
         try:
@@ -79,8 +87,6 @@ class SearchProvider:
             self.vectordb.cursor.execute(query, (document_id, input_query_embedding, num_segments,))
             query_result = self.vectordb.cursor.fetchall()
             results = [dict(zip(fields, row)) for row in query_result]
-            for result in results:
-                del result['embedding']
             return {'count': len(results), 'results': results}
         except psycopg2Error as e:
             logging.error(f"Error executing query: {e}")
@@ -92,7 +98,7 @@ class SearchProvider:
             self.vectordb.disconnect()
 
     def get_relevant_segments_by_file_id(self, input_query: str, file_id: int, num_segments: int = 5, embedding_column: str = 'embedding'):
-        fields = ['id', 'document_id', 'content', 'created_at', embedding_column]
+        fields = ['id', 'document_id', 'content', 'created_at']
         segments_query = """
             SELECT segment.id, document_id, segment.content, segment.created_at, {embedding_column}
             FROM segment
@@ -105,11 +111,18 @@ class SearchProvider:
             ORDER BY {embedding_column} <-> CAST(%s AS vector)
             LIMIT %s
         """
-        query = sql.SQL(search_query).format(
-            segments_query=sql.SQL(segments_query).format(
+        select_query = """
+            SELECT {fields}
+            FROM ({search_query}) AS relevant_segments_to_return
+        """
+        query = sql.SQL(select_query).format(
+            fields=sql.SQL(',').join(map(sql.Identifier, fields)),
+            search_query=sql.SQL(search_query).format(
+                segments_query=sql.SQL(segments_query).format(
+                    embedding_column=sql.Identifier(embedding_column)
+                ),
                 embedding_column=sql.Identifier(embedding_column)
-            ),
-            embedding_column=sql.Identifier(embedding_column)
+            )
         )
 
         try:
@@ -118,8 +131,96 @@ class SearchProvider:
             self.vectordb.cursor.execute(query, (file_id, input_query_embedding, num_segments,))
             query_result = self.vectordb.cursor.fetchall()
             results = [dict(zip(fields, row)) for row in query_result]
-            for result in results:
-                del result[embedding_column]
+            return {'count': len(results), 'results': results}
+        except psycopg2Error as e:
+            logging.error(f"Error executing query: {e}")
+            logging.error(f"Query: {query.as_string(self.vectordb.cursor)}")
+            raise e
+        except Exception as e:
+            raise e
+        finally:
+            self.vectordb.disconnect()
+
+    def get_relevant_segments_by_collection_id(self, input_query: str, collection_id: int, num_segments: int = 5, embedding_column: str = 'embedding'):
+        fields = ['id', 'document_id', 'content', 'created_at']
+        segments_query = """
+            SELECT segment.id, document_id, segment.content, segment.created_at, {embedding_column}
+            FROM segment
+            JOIN document ON segment.document_id = document.id
+            JOIN file ON document.file_id = file.id
+            WHERE file.collection_id = %s
+        """
+        search_query = """
+            SELECT *
+            FROM ({segments_query}) AS relevant_segments
+            ORDER BY {embedding_column} <-> CAST(%s AS vector)
+            LIMIT %s
+        """
+        select_query = """
+            SELECT {fields}
+            FROM ({search_query}) AS relevant_segments_to_return
+        """
+        query = sql.SQL(select_query).format(
+            fields=sql.SQL(',').join(map(sql.Identifier, fields)),
+            search_query=sql.SQL(search_query).format(
+                segments_query=sql.SQL(segments_query).format(
+                    embedding_column=sql.Identifier(embedding_column)
+                ),
+                embedding_column=sql.Identifier(embedding_column)
+            )
+        )
+
+        try:
+            input_query_embedding = self.llm.get_embedding(input_query)
+            self.vectordb.connect()
+            self.vectordb.cursor.execute(query, (collection_id, input_query_embedding, num_segments,))
+            query_result = self.vectordb.cursor.fetchall()
+            results = [dict(zip(fields, row)) for row in query_result]
+            return {'count': len(results), 'results': results}
+        except psycopg2Error as e:
+            logging.error(f"Error executing query: {e}")
+            logging.error(f"Query: {query.as_string(self.vectordb.cursor)}")
+            raise e
+        except Exception as e:
+            raise e
+        finally:
+            self.vectordb.disconnect()
+
+    def get_relevant_segments_by_file_class_id(self, input_query: str, file_class_id: int, num_segments: int = 5, embedding_column: str = 'embedding'):
+        fields = ['id', 'document_id', 'content', 'created_at']
+        segments_query = """
+            SELECT segment.id, document_id, segment.content, segment.created_at, {embedding_column}
+            FROM segment
+            JOIN document ON segment.document_id = document.id
+            JOIN file ON document.file_id = file.id
+            WHERE file.file_class_id = %s
+        """
+        search_query = """
+            SELECT *
+            FROM ({segments_query}) AS relevant_segments
+            ORDER BY {embedding_column} <-> CAST(%s AS vector)
+            LIMIT %s
+        """
+        select_query = """
+            SELECT {fields}
+            FROM ({search_query}) AS relevant_segments_to_return
+        """
+        query = sql.SQL(select_query).format(
+            fields=sql.SQL(',').join(map(sql.Identifier, fields)),
+            search_query=sql.SQL(search_query).format(
+                segments_query=sql.SQL(segments_query).format(
+                    embedding_column=sql.Identifier(embedding_column)
+                ),
+                embedding_column=sql.Identifier(embedding_column)
+            )
+        )
+
+        try:
+            input_query_embedding = self.llm.get_embedding(input_query)
+            self.vectordb.connect()
+            self.vectordb.cursor.execute(query, (file_class_id, input_query_embedding, num_segments,))
+            query_result = self.vectordb.cursor.fetchall()
+            results = [dict(zip(fields, row)) for row in query_result]
             return {'count': len(results), 'results': results}
         except psycopg2Error as e:
             logging.error(f"Error executing query: {e}")
