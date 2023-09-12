f110af259ee736c68249304be8516e2a671112a9
Dan Frederick <dpfrederick@webstaurantstore.com>
2023-09-12 18:59:00 -0400

    search page init

--- app/App.js
+++ app/App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`http://localhost/search?q=${searchTerm}`);
+      const data = await response.json();
+      setSearchResults(JSON.stringify(data));
+    } catch (error) {
+      setSearchResults(`Error: ${error}`);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchBar}
+          placeholder="Search..."
+          value={searchTerm}
+          onChangeText={(text) => setSearchTerm(text)}
+        />
+        <Button title="Search" onPress={handleSearch} />
+      </View>
+      {searchResults && <Text>{searchResults}</Text>}
     </View>
   );
 }
@@ -16,5 +37,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  searchBar: {
+    borderWidth: 2,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    width: 200,
+    height: 40,
+    paddingLeft: 8,
+    paddingRight: 8,
   },
 });
