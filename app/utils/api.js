172da441f967558cb46e5c000ea2d3bf51bab8fc
Dan Frederick <dpfrederick@webstaurantstore.com>
2023-09-17 15:31:29 -0400

    init add file

--- app/utils/api.js
+++ app/utils/api.js
@@ -14,6 +14,25 @@ async function listCollections() {
   return data;
 }
 
+async function loadFile(collectionId, url) {
+  try {
+    const storedToken = await AsyncStorage.getItem('token');
+    const response = await fetch(
+      `${API_URL}/collections/${collectionId}/load_url?url=${encodeURIComponent(url)}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${storedToken}`,
+        },
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
 async function searchDocuments(query, numResults, collectionId) {
   const storedToken = await AsyncStorage.getItem('token');
   const encoded_query = encodeURIComponent(query);
@@ -38,4 +57,4 @@ async function searchQuestions(question, numResults, collectionId) {
   return data;
 }
 
-export { listCollections, searchDocuments, searchQuestions };
+export { listCollections, loadFile, searchDocuments, searchQuestions };
