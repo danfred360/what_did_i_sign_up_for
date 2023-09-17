67cebfa52996dc4cf54adcabc2c7e2ed758c93fc
Dan Frederick <dpfrederick@webstaurantstore.com>
2023-09-17 12:08:36 -0400

    init signup page in app; add cascading deletes to vectordb tables

--- app/screens/SignIn.js
+++ app/screens/SignIn.js
@@ -10,14 +10,16 @@ const SignInPage = ({ onTokenReceived }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const AUTH_URL = Constants.expoConfig.extra.AUTH_URL || 'http://localhost:8001/token'
+  const [showSignUp, setShowSignUp] = useState(false);
+  const AUTH_URL = Constants.expoConfig.extra.AUTH_URL || 'http://localhost:8001/token';
 
-  const handleSubmit = async () => {
+  const handleSignIn = async () => {
     try {
-      const response = await fetch(AUTH_URL, {
+      const response = await fetch(`${AUTH_URL}/token`, {
         method: 'POST',
         headers: {
-          'Authorization': `Basic ${base64.encode(`${username}:${password}`)}`        }
+          'Authorization': `Basic ${base64.encode(`${username}:${password}`)}`
+        }
       });
 
       if (response.status === 401) {
@@ -33,38 +35,64 @@ const SignInPage = ({ onTokenReceived }) => {
     }
   };
 
+  const handleSignUp = async () => {
+    try {
+      const response = await fetch(`${AUTH_URL}/create_user`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.status !== 200) {
+        setError('Sign up failed');
+        return;
+      }
+
+      const data = await response.json();
+      onTokenReceived(data.access_token);
+
+    } catch (error) {
+      setError('An error occurred. Please try again later.');
+    }
+  };
+
   return (
     <SafeAreaView style={mainStyles.parent_container}>
-      <Text style={mainStyles.title}>SIGN IN</Text>
+      <Pressable onPress={() => setShowSignUp(!showSignUp)}>
+        <Text style={mainStyles.toggle}>{showSignUp ? 'Show Sign In' : 'Show Sign Up'}</Text>
+      </Pressable>
+      <Text style={mainStyles.title}>{showSignUp ? 'SIGN UP' : 'SIGN IN'}</Text>
       <View style={mainStyles.form_area}>
         <View style={mainStyles.form_group}>
-        <TextInput
-          style={mainStyles.form_style}
-          autoCapitalize="none"
-          placeholder="Username"
-          value={username}
-          onChangeText={setUsername}
-        />
+          <TextInput
+            style={mainStyles.form_style}
+            autoCapitalize="none"
+            placeholder="Username"
+            value={username}
+            onChangeText={setUsername}
+          />
         </View>
         <View style={mainStyles.form_group}>
-        <TextInput
-          style={mainStyles.form_style}
-          autoCapitalize="none"
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
+          <TextInput
+            style={mainStyles.form_style}
+            autoCapitalize="none"
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
         </View>
         <View style={mainStyles.form_group}>
-        <Pressable style={mainStyles.btn} onPress={handleSubmit}>
-          <Text style={mainStyles.text}>Sign In</Text>
-        </Pressable>
-        {error ? (
-          <View style={signInStyles.errorCard}>
-            <Text style={signInStyles.errorText}>{error}</Text>
-          </View>
-        ) : null}
+          <Pressable style={mainStyles.btn} onPress={showSignUp ? handleSignUp : handleSignIn}>
+            <Text style={mainStyles.text}>{showSignUp ? 'Sign Up' : 'Sign In'}</Text>
+          </Pressable>
+          {error ? (
+            <View style={signInStyles.errorCard}>
+              <Text style={signInStyles.errorText}>{error}</Text>
+            </View>
+          ) : null}
         </View>
       </View>
     </SafeAreaView>
